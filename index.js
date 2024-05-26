b89fe7656f4ae0ed587097874e9e663233820009
Agung Firmansyah <agunzprogrammer@gmail.com>
2024-05-26 20:43:47 +0700

    nambah fitur

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-// untuk munculin navigation
+// untuk munculin navigation hamburger
 const nav_list = document.querySelector(".nav-list");
 
+
+
+
+// katika hamburger di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   nav_list.classList.toggle("active");
 };
 
-// untuk menyembunyikan navigation ketika klik di luar area navigation
-const hamburger = document.querySelector("#hamburger-menu");
+// untuk munculin searchform
+const searchform = document.querySelector(".search-form");
+const searchbox = document.querySelector("#search-box");
 
-document.addEventListener("click", function(e) {
+document.querySelector("#search-button").onclick = (e) => {
+  searchform.classList.toggle("active");
+  searchbox.focus();
+  e.preventDefault();
+};
+
+// klik luar element
+const hamburger = document.querySelector("#hamburger-menu");
+const searchbutton = document.querySelector("#search-button");
+
+document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !nav_list.contains(e.target)) {
     nav_list.classList.remove("active");
   }
+
+  if (!searchbutton.contains(e.target) && !searchform.contains(e.target)) {
+    searchform.classList.remove("active");
+  }
+
 });
